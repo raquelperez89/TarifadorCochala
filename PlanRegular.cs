3b449020f9597a4553744eac3799fabe477f9fc8
raquelperez <raquel.perez@training.assuresoft.com>
2022-08-03 17:01:55 -0400

    Agregando la Clase PlanRegular

---
+++ PlanRegular.cs
@@ -0,0 +1,15 @@
+class PlanRegular{
+    private readonly double costoMinuto;
+    private readonly double costoInicial;
+    private IVehiculo vehiculo; 
+    double planConVehiculo;
+    public PlanRegular(IVehiculo vehiculo){
+        this.costoMinuto = 0.8;
+        this.costoInicial = 5;
+        this.vehiculo = vehiculo;
+    } 
+    public double agregarMonto(){
+        //planConVehiculo = this.costoInicial + this.vehiculo.getMontoRegular();
+        return planConVehiculo;
+    }
+}
