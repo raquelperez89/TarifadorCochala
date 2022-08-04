28a78bb77e0b6acd8d048e13dbb5a52d16cd665f
AndrewTorrez <andrew.torrez@assuresoftbolivia.com>
2022-08-03 23:04:46 -0400

    se creo la clase con los atributos iniciales

---
+++ PlanMensual.cs
@@ -0,0 +1,13 @@
+public class PlanMensual
+{
+    public double costoMinuto { get; set;}
+    public double costoInicial { get; set;}
+
+    public PlanMensual(){
+        this.costoMinuto = 0.70;
+        this.costoInicial = 70;
+    }
+    public void agregarMonto(){
+        
+    }  
+}
