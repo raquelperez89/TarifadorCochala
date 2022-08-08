6dd12d4ae473f79bf266ca008e0744c2de7fa02a
raquelperez <raquel.perez@training.assuresoft.com>
2022-08-07 23:14:52 -0400

    Creacion de la clase usuario con sus atributos y metodos

---
+++ Usuario.cs
@@ -0,0 +1,8 @@
+class Usuario{
+    private int ci{get;set;}
+    private string nombre{get;set;}
+    private DateTime fechaDeNacimiento{get;set;}
+    private string ocupacion{get;set;}
+    //private Plan plan;
+
+}
