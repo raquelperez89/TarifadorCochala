ad58232cc03eefdee5756067613a3e07f0fccd2b
JuanJoseQuiroga <juanjose.quiroga@assuresoftbolivia.com>
2022-08-12 11:32:19 -0400

    refactored method and added new unit tests in to Registro

--- Tarifador/Registro.cs
+++ Tarifador/Registro.cs
@@ -1,22 +1,33 @@
 
 public class Registro
 {
-    public IVehiculo ? vehiculo {get; set;}
-    public int minutos {get; set;}
-    public DateOnly fecha {get; set;} 
-    public double montoRegistro {get; set;} = 0;
+    public IVehiculo? vehiculo { get; set; }
+    public int minutos { get; set; }
+    public DateOnly fecha { get; set; }
+    public double montoRegistro { get; set; } = 0;
 
-    public Registro( IVehiculo vehiculo, int minutos){
+    private double limiteDescuento = 0.50;
+
+    public Registro(IVehiculo vehiculo, int minutos)
+    {
         this.fecha = DateOnly.FromDateTime(DateTime.Now);
         this.vehiculo = vehiculo;
         this.minutos = minutos;
     }
 
-    public void calcularMontoRegistro(Usuario usuario, List<IDescuento> descuentos, IImpuesto impuesto)
+    private double limitarDescuentos(double limiteDescuento, double totalDescuentos)
+    {
+        if (totalDescuentos > limiteDescuento)
+        {
+            return limiteDescuento;
+        }
+        return totalDescuentos;
+    }
+    public void calcularMontoRegistro(Usuario usuario, List<IDescuento> descuentos)
     {
         double totalDescuentos = descuentos.Aggregate(0.0, (sumaDescuntos, descuento) => sumaDescuntos + descuento.getPorcentajeDescuento(usuario));
-        montoRegistro = usuario.plan.cobroTotalPorMinuto(this.vehiculo) * minutos ;
+        totalDescuentos = limitarDescuentos(limiteDescuento, totalDescuentos);
+        montoRegistro = usuario.plan.cobroTotalPorMinuto(this.vehiculo) * minutos;
         montoRegistro = montoRegistro - montoRegistro * totalDescuentos;
-        montoRegistro = impuesto.applicarImpuesto(montoRegistro);
     }
 }
--- TarifadorTests/RegistroTest.cs
+++ TarifadorTests/RegistroTest.cs
@@ -23,11 +23,60 @@ namespace TarifadorTests
             List<IDescuento> descuentos = new List<IDescuento>(){
             new DescuentoCumple(),
             new DescuentoEstudiante()};
-            
-             registro.calcularMontoRegistro(usuario, descuentos, impuesto);
+
+            registro.calcularMontoRegistro(usuario, descuentos);
+            double montoTotal = registro.montoRegistro;
+
+            Assert.AreEqual(7.0, montoTotal);
+        }
+
+        [Test]
+        public void calcularMontoRegistroEstudiante()
+        {
+            PlanRegular planRegular = new PlanRegular();
+            Usuario usuario = new Usuario(1, "Pedro", new DateOnly(2000, 08, 10), "Estudiante", planRegular);
+            List<IDescuento> descuentos = new List<IDescuento>(){
+            new DescuentoCumple(),
+            new DescuentoEstudiante()};
+
+            registro.calcularMontoRegistro(usuario, descuentos);
+            double montoTotal = registro.montoRegistro;
+
+            Assert.AreEqual(5.95, montoTotal);
+        }
+
+        [Test]
+        public void calcularMontoRegistroEstudianteConDescuentoCumpleaño()
+        {
+            PlanRegular planRegular = new PlanRegular();
+            Usuario usuario = new Usuario(1, "Pedro", new DateOnly(2000, 08, 12), "Estudiante", planRegular);
+            List<IDescuento> descuentos = new List<IDescuento>(){
+            new DescuentoCumple(),
+            new DescuentoEstudiante()};
+
+            registro.calcularMontoRegistro(usuario, descuentos);
+            double montoTotal = registro.montoRegistro;
+
+            Assert.AreEqual(3.8500000000000005d, montoTotal);
+        }
+
+        [Test]
+        public void calcularMontoRegistroConDescuentoDel90PorCiento()
+        {
+            PlanRegular planRegular = new PlanRegular();
+            Usuario usuario = new Usuario(1, "Pedro", new DateOnly(2000, 08, 12), "Estudiante", planRegular);
+            List<IDescuento> descuentos = new List<IDescuento>(){
+            new DescuentoCumple(),
+            new DescuentoEstudiante(),
+            new DescuentoEstudiante(),
+            new DescuentoEstudiante(),
+            new DescuentoEstudiante()};
+
+            registro.calcularMontoRegistro(usuario, descuentos);
             double monto = registro.montoRegistro;
 
-            Assert.AreEqual(7.0, monto);
+            Assert.AreEqual(3.5, monto);
         }
+        
     }
 }
