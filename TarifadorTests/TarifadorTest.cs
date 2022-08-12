755b4b70ca29c97f1bca425f9d8f5ce6b396ec95
JuanJoseQuiroga <juanjose.quiroga@assuresoftbolivia.com>
2022-08-12 13:17:26 -0400

    conflicts where solved

--- TarifadorTests/TarifadorTest.cs
+++ TarifadorTests/TarifadorTest.cs
@@ -14,31 +14,34 @@ namespace TarifadorTests
         }
 
         [Test]
-        public void addUsuarioTest(){
+        public void addUsuarioTest()
+        {
             IPlan plan = new PlanMensual();
-            DateOnly fechaNacimiento = new DateOnly(2000,12,1);
+            DateOnly fechaNacimiento = new DateOnly(2000, 12, 1);
             Usuario usuario = new Usuario(89898, "Juan", fechaNacimiento, "Estudiante", plan);
             tarifador.addUsuario(usuario);
-            Assert.AreEqual(89898,tarifador.getListaUsuarios()[0].ci);
+            Assert.AreEqual(89898, tarifador.getListaUsuarios()[0].ci);
             Assert.AreEqual("Juan", tarifador.getListaUsuarios()[0].nombreCompleto);
             Assert.AreEqual(fechaNacimiento, tarifador.getListaUsuarios()[0].fechaNacimiento);
-            Assert.AreEqual("Estudiante",tarifador.getListaUsuarios()[0].ocupacion);
-            Assert.AreEqual(89898,tarifador.getListaUsuarios()[0].ci);
-            Assert.IsInstanceOf(typeof(IPlan),tarifador.getListaUsuarios()[0].plan);
+            Assert.AreEqual("Estudiante", tarifador.getListaUsuarios()[0].ocupacion);
+            Assert.AreEqual(89898, tarifador.getListaUsuarios()[0].ci);
+            Assert.IsInstanceOf(typeof(IPlan), tarifador.getListaUsuarios()[0].plan);
 
         }
 
         [Test]
-        public void getListaUsuariosTest(){
-            Assert.IsInstanceOf(typeof(List<Usuario>),tarifador.getListaUsuarios());
+        public void getListaUsuariosTest()
+        {
+            Assert.IsInstanceOf(typeof(List<Usuario>), tarifador.getListaUsuarios());
 
         }
 
         [Test]
-        public void addListaUsuariosTest(){
+        public void addListaUsuariosTest()
+        {
             List<Usuario> listaUsuariosSinAgregar = this.tarifador.getListaUsuario();
             IPlan plan = new PlanMensual();
-            DateOnly fechaNacimiento = new DateOnly(2000,12,1);
+            DateOnly fechaNacimiento = new DateOnly(2000, 12, 1);
             List<Usuario> listaUsuarios = new List<Usuario>()
             {
                 new Usuario(8988, "Test2", fechaNacimiento, "Estudiante", plan)
@@ -48,12 +51,14 @@ namespace TarifadorTests
         }
 
         [Test]
-        public void tarifarTest(){
+        public void tarifarTest()
+        {
             List<Usuario> usuarios = new List<Usuario>();
         }
 
         [Test]
-        public void aniadirRegistroAUsuarioTest(){
+        public void aniadirRegistroAUsuarioTest()
+        {
             IVehiculo vehiculo = new Bicicleta();
             Registro registro = new Registro(vehiculo, 30);
             IPlan plan = new PlanMensual();
@@ -70,21 +75,17 @@ namespace TarifadorTests
             tarifador.addListaUsuarios(listaUsuarios);
             tarifador.aniadirRegistroAUsuario(89898, registro);
             tarifador.aniadirRegistroAUsuario(89898, registro);
-            listaUsuarios[1].calcularMontoTotalRegistros(descuento, impuesto);
-
-            List<Registro> listaRegistros = new List<Registro>();
-            double result = listaUsuarios[1].montoPorPagar;
-
-            Assert.AreNotEqual(listaRegistros.Count, listaUsuarios[1].listaRegistros.Count);
-            Assert.AreEqual(35.19, result);
+            
+            Assert.AreNotEqual(listaRegistros.Count,listaUsuarios[1].listaRegistros.Count);
         }
 
         [Test]
-        public void aniadirRegistroAUsuarioNullTest(){
+        public void aniadirRegistroAUsuarioNullTest()
+        {
             IVehiculo vehiculo = new Bicicleta();
             Registro registro = new Registro(vehiculo, 30);
             IPlan plan = new PlanMensual();
-            DateOnly fechaNacimiento = new DateOnly(2000,12,1);
+            DateOnly fechaNacimiento = new DateOnly(2000, 12, 1);
             List<Usuario> listaUsuarios = new List<Usuario>()
             {
                 new Usuario(1, "Pedro picapiedra", new DateOnly(2000, 08, 11), "plomero", plan),
@@ -93,7 +94,68 @@ namespace TarifadorTests
             List<Registro> listaRegistros = new List<Registro>();
             tarifador.addListaUsuarios(listaUsuarios);
             tarifador.aniadirRegistroAUsuario(00, registro);
-            Assert.AreEqual(listaRegistros.Count,listaUsuarios[1].listaRegistros.Count);
+            Assert.AreEqual(listaRegistros.Count, listaUsuarios[1].listaRegistros.Count);
+        }
+
+
+        [Test]
+        public void tarifarConListaDeDescuentos()
+        {
+            IVehiculo vehiculo = new Scooter();
+            Registro registro = new Registro(vehiculo, 30);
+            IPlan plan = new PlanRegular();
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+            List<Usuario> listaUsuarios = new List<Usuario>()
+            {
+            new Usuario(1, "Pedro", today, "plomero", plan),
+            new Usuario(89898, "goku", new DateOnly(2000, 04, 20), "Estudiante", plan)
+            };
+            List<Registro> listaRegistros = new List<Registro>();
+            listaRegistros.Add(registro);
+            listaRegistros.Add(registro);
+            tarifador.addListaUsuarios(listaUsuarios);
+            tarifador.aniadirRegistroAUsuario(listaUsuarios[0].ci, listaRegistros[0]);
+            tarifador.aniadirRegistroAUsuario(listaUsuarios[1].ci, listaRegistros[1]);
+
+            List<IDescuento> descuentos = new List<IDescuento>(){
+                new DescuentoCumple(),
+                new DescuentoEstudiante()
+            };
+            tarifador.tarifar(descuentos);
+            var output = new StringWriter();
+            Console.SetOut(output);
+            tarifador.mostrarMontoPorUsuario();
+            Assert.That(output.ToString(), Is.EqualTo(string.Format(listaUsuarios[0].nombreCompleto + ", deuda: " + "14,7" + "\r\n" +
+                                                                    listaUsuarios[1].nombreCompleto + ", deuda: " + "17,85" + "\r\n")));
+        }
+
+        [Test]
+        public void tarifarConListaDeDescuentosVacia()
+        {
+            IVehiculo vehiculo = new Scooter();
+            Registro registro = new Registro(vehiculo, 30);
+            IPlan plan = new PlanRegular();
+            DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+            List<Usuario> listaUsuarios = new List<Usuario>()
+            {
+            new Usuario(1, "Pedro", today, "plomero", plan),
+            new Usuario(89898, "goku", new DateOnly(2000, 04, 20), "Estudiante", plan)
+            };
+            List<Registro> listaRegistros = new List<Registro>();
+            listaRegistros.Add(registro);
+            listaRegistros.Add(registro);
+            tarifador.addListaUsuarios(listaUsuarios);
+            tarifador.aniadirRegistroAUsuario(listaUsuarios[0].ci, listaRegistros[0]);
+            tarifador.aniadirRegistroAUsuario(listaUsuarios[1].ci, listaRegistros[1]);
+
+            List<IDescuento> descuentos = new List<IDescuento>(){
+            };
+            tarifador.tarifar(descuentos);
+            var output = new StringWriter();
+            Console.SetOut(output);
+            tarifador.mostrarMontoPorUsuario();
+            Assert.That(output.ToString(), Is.EqualTo(string.Format(listaUsuarios[0].nombreCompleto + ", deuda: " + "21" + "\r\n" +
+                                                                    listaUsuarios[1].nombreCompleto + ", deuda: " + "21" + "\r\n")));
         }
         [Test]
         public void mostrarMontoPorUsuarioTest(){
